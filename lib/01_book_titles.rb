5a316a832c99bd4cb093b0c1a486dbe4dfd11585
Zuhair A. Shaikh <zuhair@Zuhairs-MacBook-Air.local>
2017-05-08 17:31:26 -0700

    one-third completion commit

--- lib/01_book_titles.rb
+++ lib/01_book_titles.rb
@@ -1,3 +1,16 @@
 class Book
-  # TODO: your code goes here!
+
+  attr_reader :title
+
+  def title=(title)
+    lowercase_words = %w(the a an of and in)
+    @title = title.split.map.with_index do |word, idx|
+      if idx == 0
+        word.capitalize
+      else
+        lowercase_words.include?(word) ? word : word.capitalize
+      end
+    end.join(" ")
+  end
+
 end
